db78375c12eb50317c71b382d8944d769c19060e
AK-Bishnu <aronnoghosh049@gmail.com>
2025-09-16 00:07:24 +0600

    Update settings.py for Render deployment

--- Ak Fashion/ak_fashion_django/project/project/settings.py
+++ Ak Fashion/ak_fashion_django/project/project/settings.py
@@ -9,7 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ng66(j%!m-qm*%@juq3dxrm93!7!f06_m45p+1&y(qn$f%atpt'
+# Get secret key and debug from environment
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

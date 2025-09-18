28aa7b72ab97c0a4b7425181328a4caa823b6d35
AK-Bishnu <aronnoghosh049@gmail.com>
2025-09-18 14:22:28 +0600

    Using Supabase

--- Ak Fashion/ak_fashion_django/project/project/settings.py
+++ Ak Fashion/ak_fashion_django/project/project/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -70,12 +71,24 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        'postgres://postgres:1290qwop@db.ltcsfqgsqulunlfjykpa.supabase.co:5432/postgres',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 
 
 # Password validation
